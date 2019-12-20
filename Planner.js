c68b05844cb49868a4422ccc6cb17422dab06864
Brad Bryant <bradleybryant86@gmail.com>
2019-12-20 15:17:13 -0600

    Created hour IDs in planner.js in order to save to local storage

--- Planner.js
+++ Planner.js
@@ -1,8 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-// Save inputs from textboxes
+    debugger;
 
-    $(".saveButton").on("click", function () {
+    // Save inputs from textboxes
+
+    $(".saveButton").on("click", function (event) {
+        event.preventDefault();
         var toDo = $(this).siblings(".hourly-tasks").val()
         var time = $(this).siblings().attr("id")
         // var time = $(this).siblings(".input-group-text").val()
@@ -10,9 +13,42 @@ $(document).ready(function () {
         console.log(time);
 
         // Save into local storage
-        localStorage.setItem(toDo, time);
+        localStorage.setItem(time, toDo);
 
         // Prevent saved input from being erased when page is refreshed.
+ 
 
-    }
+    })
 
+    $("#hour-9").text(localStorage.getItem("9am"));
+    var description = localStorage.getItem("9am");
+    $("hour-10").text(localStorage.getItem("10am"));
+    var description = localStorage.getItem("10am");
+    $("hour-11").text(localStorage.getItem("11am"));
+    var description = localStorage.getItem("11am");
+    $("hour-12").text(localStorage.getItem("12am"));
+    var description = localStorage.getItem("12am");
+    $("hour-1").text(localStorage.getItem("1pm"));
+    var description = localStorage.getItem("1pm");
+    $("hour-2").text(localStorage.getItem("2pm"));
+    var description = localStorage.getItem("2pm");
+    $("hour-3").text(localStorage.getItem("3pm"));
+    var description = localStorage.getItem("3pm");
+    $("hour-4").text(localStorage.getItem("4pm"));
+    var description = localStorage.getItem("4pm");
+    $("hour-5").text(localStorage.getItem("5pm"));
+    var description = localStorage.getItem("5pm");
+
+    function renderDescription(time) ;
+   
+    console.log(time);
+  
+    //     // var description = localStorage.getItem(time);
+    //     // console.log(description);
+    //     $(".hourly-tasks").text(description);
+    // }
+
+
+
+    // renderDescription(description); 
+});
